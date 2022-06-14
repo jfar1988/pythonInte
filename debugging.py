64471a8fdf0aeb8576980b4fd2f765f5b0adb86f
jfar1988 <69083615+jfar1988@users.noreply.github.com>
2022-06-14 14:07:53 -0500

    Practicando try except finally

--- debugging.py
+++ debugging.py
@@ -1,13 +1,28 @@
-from prueba import run
+# from prueba import run
 
-def divisors(num):
-    divisors = [i for i in range(1, num +1) if num % i == 0]
-    return divisors
+# def divisors(num):
+#     divisors = [i for i in range(1, num +1) if num % i == 0]
+#     return divisors
 
-def run():
-    num = int(input("Ingresa un numero: "))
-    print(divisors(num))
-    print("Termino el programa")
+# def run():
+    # num = int(input("Ingresa un numero: "))
+    # print(divisors(num))
+    # print("Termino el programa")
+def palindrome(string):
+
+
+    try:
+        if len(string) == 0:
+            raise ValueError("No se puede ingresar una cadena de texto vacia")
+        return string == string[::-1]
+    except ValueError as ve:
+        print(ve)
+        return False
+
+try:
+    print(palindrome(""))
+except TypeError:
+    print("Solo se puede ingresar String o Cadenas de texto")
 
 if "__main__" == __name__:
-    run()
+    palindrome("")
